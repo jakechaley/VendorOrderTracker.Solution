91fc8dd598decde81b6275d0f3d55f5753f36906
jakechaley <jakechaley@gmail.com>
2022-03-04 09:32:14 -0800

    first test passes already becasue constructor exists already

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class OrderTests
+  {
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
