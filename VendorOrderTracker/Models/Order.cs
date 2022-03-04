3258e2e3683f2c0524487ba704479138c813cb3a
jakechaley <jakechaley@gmail.com>
2022-03-04 13:55:41 -0800

    Pass test for Get id

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -9,7 +9,7 @@ namespace VendorOrderTracker.Models
     public string Description { get; set; }
     public int Price { get; set; }
     public DateTime Date { get; set; } 
-    public int Id {}
+    public int Id { get; }
 
     private static List<Order> _instances = new List<Order> { };
     public Order(string title, string description, int price, DateTime date)
