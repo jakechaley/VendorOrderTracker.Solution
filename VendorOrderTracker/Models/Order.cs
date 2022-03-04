c3c289135229d777224aec40952dd70565da220a
jakechaley <jakechaley@gmail.com>
2022-03-04 10:52:36 -0800

    pass SetDescription test

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,7 +5,7 @@ namespace VendorOrderTracker.Models
   public class Order
   {
     public string Title { get; set; }
-    public string Description { get; }
+    public string Description { get; set; }
     private static List<Order> _instances = new List<Order> {};
     public Order(string title, string description)
     {
