99119d488fd4669c5f5640d6c02cb1749d02c997
jakechaley <jakechaley@gmail.com>
2022-03-04 15:29:10 -0800

    Pass findMeethod test

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -32,7 +32,7 @@ namespace VendorOrderTracker.Models
 
     public static Vendor Find(int searchId)
     {
-      return _instances[searchId - 2];
+      return _instances[searchId - 1];
     }
   }
 }
