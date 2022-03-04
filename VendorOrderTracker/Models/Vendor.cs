8c66aa1885b968b23c0a5b61ef91d4081a4fbc94
jakechaley <jakechaley@gmail.com>
2022-03-04 14:35:47 -0800

    pass Constructor test

--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -4,6 +4,16 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor 
   {
-    
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name;
+    public string Description;
+    public List<Order> Orders;
+
+    public Vendor(string vendorName)
+    {
+      Name = vendorName;
+      _instances.Add(this);
+      Orders = new List<Order>{};
+    }
   }
 }
